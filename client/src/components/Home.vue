072f9e552b9fa3008caa1896f6d05701fe4e6b4b
Tetyana Shvets <tshvets@hotmail.com>
2019-06-02 15:52:54 -0700

    Integrate Vuex with ApolloClient

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,17 +1,38 @@
 <template>
-  <v-container
-    text-xs-center
-    v-if="getPosts"
-  >
+  <v-container text-xs-center>
+    <v-layout row>
+      <v-dialog
+        v-model="loading"
+        persistent
+        fullscreen
+      >
+        <v-container fill-height>
+          <v-layout
+            row
+            justify-center
+            align-center
+          >
+            <v-progress-circular
+              indeterminate
+              :size="70"
+              :width="7"
+              color="secondary"
+            ></v-progress-circular>
+          </v-layout>
+        </v-container>
+      </v-dialog>
+    </v-layout>
+
     <v-flex xs12>
       <v-carousel
+        v-if="!loading && posts.length > 0"
         v-bind="{ 'cycle': true }"
-        interval='3000'
+        interval="3000"
       >
         <v-carousel-item
-          v-for='post in getPosts'
-          :key='post._id'
-          :src='post.imageUrl'
+          v-for="post in posts"
+          :key="post._id"
+          :src="post.imageUrl"
         >
           <h1 id="carousel__title">{{post.title}}</h1>
         </v-carousel-item>
@@ -21,26 +42,19 @@
 </template>
 
 <script>
-import { gql } from "apollo-boost";
+import { mapGetters } from "vuex";
+
 export default {
   name: "home",
-  apollo: {
-    getPosts: {
-      query: gql`
-        query {
-          getPosts {
-            _id
-            title
-            imageUrl
-            description
-            categories
-            likes
-            createdBy {
-              username
-            }
-          }
-        }
-      `
+  created() {
+    this.handleGetCarouselPosts();
+  },
+  computed: {
+    ...mapGetters(["loading", "posts"])
+  },
+  methods: {
+    handleGetCarouselPosts() {
+      this.$store.dispatch("getPosts");
     }
   }
 };
